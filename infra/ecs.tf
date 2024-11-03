07252de79972ebfc54bde4decc9e777bceb0442f
Tigran Khachatryan <tigran.kh99.aws@gmail.com>
2024-11-03 19:50:25 +0400

    ECS deployment in infra :)

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -1,90 +1,34 @@
-resource "aws_subnet" "ecs_subnet" {
-  vpc_id            = aws_vpc.core.id
-  cidr_block        = "10.0.100.0/24"
-}
-
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "${terraform.workspace}-infra-cluster"
-
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-
-  tags = {
-    Name = "${terraform.workspace}-infra-cluster"
-  }
+  name = "${terraform.workspace}-core-cluster"
 }
 
 resource "aws_ecs_task_definition" "ecs_task" {
-  family                   = "my-ecs-task"
-  network_mode             = "bridge"
-  requires_compatibilities = ["EC2"]
+  family                   = "${terraform.workspace}-web-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
 
-  container_definitions = jsonencode([
-    {
-      name      = "app1-container"
-      image     = "nginx:latest"
-      cpu       = 128
-      memory    = 256
-      essential = true
-      portMappings = [{
-        containerPort = 80
-        hostPort      = 80
-      }]
-    },
-    {
-      name      = "app2-container"
-      image     = "httpd:latest"
-      cpu       = 128
-      memory    = 256
-      essential = true
-      portMappings = [{
-        containerPort = 80
-        hostPort      = 8080
-      }]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name      = "${terraform.workspace}-web-app01"
+    image     = "nginx:latest"  # Replace with your own image if needed
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
 }
 
-# ECS Service
 resource "aws_ecs_service" "ecs_service" {
-  name            = "my-ecs-service"
+  name            = "${terraform.workspace}-web-app01-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.ecs_task.arn
-  desired_count   = 2
-
-  launch_type = "EC2"
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  
+  network_configuration {
+    subnets         = aws_subnet.core_private[*].id
+    assign_public_ip = true
+  }
 }
-
-
-
-# IAM Role for ECS Instances
-resource "aws_iam_role" "ecs_instance_role" {
-  name = "${terraform.workspace}-infra-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Principal = { Service = "ec2.amazonaws.com" },
-      Effect    = "Allow"
-    }]
-  })
-}
-
-# Attach policies to the IAM role
-resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
-  role       = aws_iam_role.ecs_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-
-# IAM Instance Profile
-resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecs_instance_profile"
-  role = aws_iam_role.ecs_instance_role.name
-}
-
