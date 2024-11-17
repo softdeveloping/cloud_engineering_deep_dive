02bac11683e374a00e0c9896cbe63d82f9392be2
Tigran Khachatryan <tigran.kh99.aws@gmail.com>
2024-11-17 18:06:24 +0400

    Deploy frontend example

--- infra/front.tf
+++ infra/front.tf
@@ -48,7 +48,7 @@ resource "aws_cloudfront_distribution" "frontend" {
 
   origin {
     domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
-    origin_id = "S3Oirgin"
+    origin_id = "S3Origin"
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.frontend.cloudfront_access_identity_path
@@ -56,8 +56,8 @@ resource "aws_cloudfront_distribution" "frontend" {
   }
 
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3Origin"
 
     forwarded_values {
