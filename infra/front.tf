ca724dcade043db23e830848ba71e7d8562fcef5
Tigran Khachatryan <tigran.kh99.aws@gmail.com>
2024-11-17 17:24:43 +0400

    Deploy frontend example

--- infra/front.tf
+++ infra/front.tf
@@ -66,8 +66,7 @@ resource "aws_cloudfront_distribution" "frontend" {
         forward = "none"
       }
     }
-
-    viewer_protocol_policy = "redirect_to_https"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl = 0
     default_ttl = 3600
     max_ttl = 86400
