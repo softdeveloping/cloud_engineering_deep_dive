1c27ef1b021ab62faa9c65362612484f9f1d885e
Tigran Khachatryan <tigran.kh99.aws@gmail.com>
2024-12-03 22:59:13 +0400

    Lambda homework doing

--- main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "p-trfm-state-back.demo"
-    dynamodb_table = "p-trfm-state-lock.demo"
-    encrypt        = true
-    key            = "projects/p-trfm-state-back.demo.tfstate"
-    region         = "us-east-1"
-  }
-}
